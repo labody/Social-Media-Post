0c219b9a48f7303fed565c25d3005f5fc327c811
labody <applemafia69@yahoo.com.com>
2020-03-02 13:00:47 +0000

    Increased size of the post

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -19,104 +20,110 @@ class MyApp extends StatelessWidget {
           ],
         ),
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(12.0),
-                height: 600.0,
-                padding: EdgeInsets.all(13.0),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: AssetImage('images/mou.jpg'),
-                          radius: 30.0,
-                        ),
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(right: 120.0, top: 15.0),
-                          child: Text(
-                            'Jose Mourinho',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(12.0),
+                  height: 800.0,
+                  padding: EdgeInsets.all(13.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: AssetImage('images/mou.jpg'),
+                            radius: 30.0,
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: Icon(Icons.more_vert),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
-                      child: Text(
-                          'The best duo in the NBA (Russell Westbrook and James Harden) laughing at the comments on the beef between James and Giannis.',
-                          style: TextStyle(fontSize: 17.0)),
-                    ),
-                    Expanded(
-                      child: ClipRRect(
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 120.0, top: 15.0),
+                            child: Text(
+                              'Jose Mourinho',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20.0),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Icon(Icons.more_vert),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                        child: Text(
+                            'The best duo in the NBA (Russell Westbrook and James Harden) laughing at the comments on the beef between James and Giannis.',
+                            style: TextStyle(fontSize: 17.0)),
+                      ),
+                      ClipRRect(
                           borderRadius: BorderRadius.circular(15.0),
-                          child: Image.asset('images/duo.webp')),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        SizedBox(height: 12.0),
-                        IconButton(
-                          icon: Icon(Icons.favorite),
-                          color: Colors.red,
-                          onPressed: () {},
-                        ),
-                        Text('130K'),
-                        IconButton(
-                            icon: Icon(Icons.message),
-                            color: Colors.indigoAccent,
-                            onPressed: () {}),
-                        Text('896'),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 100.0),
-                          child: Text('12 hours ago'),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(left: 110.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20.0),
-                            child: RaisedButton.icon(
+                          child: Image.asset(
+                            'images/duo.webp',
+                            //scale: 4,
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(height: 12.0),
+                            IconButton(
                               icon: Icon(Icons.favorite),
+                              color: Colors.red,
                               onPressed: () {},
-                              label: Text('Like'),
-                              color: Colors.amberAccent,
+                            ),
+                            Text('130K'),
+                            IconButton(
+                                icon: Icon(Icons.message),
+                                color: Colors.indigoAccent,
+                                onPressed: () {}),
+                            Text('896'),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 100.0),
+                              child: Text('12 hours ago'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 110.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: RaisedButton.icon(
+                                icon: Icon(Icons.favorite),
+                                onPressed: () {},
+                                label: Text('Like'),
+                                color: Colors.amberAccent,
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 25.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20.0),
-                            child: RaisedButton.icon(
-                              icon: Icon(Icons.question_answer),
-                              onPressed: () {},
-                              label: Text('Comment'),
-                              color: Colors.amberAccent,
+                          Padding(
+                            padding: const EdgeInsets.only(left: 25.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: RaisedButton.icon(
+                                icon: Icon(Icons.question_answer),
+                                onPressed: () {},
+                                label: Text('Comment'),
+                                color: Colors.amberAccent,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
